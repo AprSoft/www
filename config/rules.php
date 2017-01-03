dda8b523d8a93429b9e7155920603b31a25ff70a
张欣宇 <176956856@qq.com>
2017-01-03 16:47:57 +0800

    添加文章控制器。

--- config/rules.php
+++ config/rules.php
@@ -1,4 +1,5 @@
 <?php
 return [
-    
+    '/' => 'site/index',
+    'article\/' => 'article/index'
 ];
---
+++ controllers/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class ArticleController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
